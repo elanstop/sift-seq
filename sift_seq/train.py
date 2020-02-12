823ee1c6eb57ba9b8209f4a3e6cd419b311656ac
elanstop <e.stopnitzky@gmail.com>
2020-02-11 16:08:48 -0800

    updated paths

--- sift_seq/train.py
+++ sift_seq/train.py
@@ -11,10 +11,10 @@ from sift_seq.experimental_model import FragmentClassifier
 # the encoded reads file are intermediate files which are saved for potential later use, because one-hot encoding the
 # sequences takes some time
 
-ViralGenomeData("data/training_viral_genomes.fasta", 'data/encoded_viral_reads.txt')
-HumanGenomeData('data/human.fa', 'data/encoded_human_reads.txt')
-BacterialGenomeData("data/training_bacterial_genomes.fasta", "data/encoded_bacterial_reads.txt")
+ViralGenomeData("/data/training_viral_genomes.fasta", '/data/encoded_viral_reads.txt')
+HumanGenomeData('/data/human.fa', '/data/encoded_human_reads.txt')
+BacterialGenomeData("/data/training_bacterial_genomes.fasta", "/data/encoded_bacterial_reads.txt")
 
 # num_data refers to how many points should be pulled from each of the three files
-FragmentClassifier('data/encoded_viral_reads.txt', 'data/encoded_human_reads.txt', 'data/encoded_bacterial_reads.txt',
+FragmentClassifier('/data/encoded_viral_reads.txt', '/data/encoded_human_reads.txt', '/data/encoded_bacterial_reads.txt',
                    read_length=100, num_data=10, test_fraction=0.2, epochs=10, batch_size=100)
